650862c437c0151c965dfd1445fdcf6d75d1854e
markeetas <markeetas@gmail.com>
2024-04-17 21:55:54 +0200

    nav bar with logo

--- pojisteni/urls.py
+++ pojisteni/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, url_handlers
 
 urlpatterns = [
     path("insured_index/", views.InsuredIndex.as_view(), name="insured_index"),
     path("<int:pk>/insured_detail/", views.CurrentInsured.as_view(), name="insured_detail"),
     path("create_insured/", views.CreateInsured.as_view(), name="novy_pojisteny"),
-    # path("insurance_index/, views.")
+    path("insurance_index/", views.InsuranceIndex.as_view(), name="insurance_index"),
+    path("about/", views.About.as_view(), name="about"),
+    path("events_index/", views.EventsIndex.as_view(), name="events_index"),
+    path('', views.HomeView.as_view(), name='home'),
+    path("", url_handlers.index_handler),
 ]
+
--- pojisteni/views.py
+++ pojisteni/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Insurance, Insured, InsuranceEvent, UserRole, Statistics
 from .forms import InsuredForm
+from django.views.generic.base import TemplateView
 
 
 class InsuredIndex(generic.ListView):
@@ -11,6 +12,14 @@ class InsuredIndex(generic.ListView):
 
     def get_queryset(self):
         return Insured.objects.all()
+    
+    
+class InsuranceIndex(generic.ListView):
+    template_name = "pojisteni/insurance_index.html"
+    context_object_name = "insurances"
+
+    def get_queryset(self):
+        return Insurance.objects.all()    
     
 class CurrentInsured(generic.DetailView):
 
@@ -33,3 +42,16 @@ class CreateInsured(generic.edit.CreateView):
             form.save()
         
         return render(request, self.template_name, {"form": form})  
+    
+class About(generic.TemplateView):
+    template_name = "pojisteni/about.html"    
+    
+class EventsIndex(generic.ListView):  # přidáno
+    template_name = "pojisteni/events_index.html"
+    context_object_name = "events"
+
+    def get_queryset(self):
+        return InsuranceEvent.objects.all()    
+    
+class HomeView(TemplateView):
+    template_name = 'pojisteni/home.html'    
